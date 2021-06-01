39e2c5ac8b990a7b569c00e46c61d8959369500c
Admin <isaacbest.ig@gmail.com>
2021-06-02 00:33:59 +0700

    up

---
+++ navbar_table_list.php
@@ -0,0 +1,17 @@
+<nav class="navbar navbar-expand-lg navbar-light bg-light">
+    <a class="navbar-brand" href="table_list.php">Danh Sách Sản Phẩm</a>
+    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+    </button>
+
+    <div class="collapse navbar-collapse" id="navbarSupportedContent">
+        <ul class="navbar-nav mr-auto">
+            <li class="nav-item active">
+                <a class="nav-link" href="themSP.php">Thêm Sản Phẩm <span class="sr-only">(current)</span></a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="suaSP.php">Sửa Sản Phẩm</a>
+            </li>
+        </ul>
+    </div>
+</nav>
--- themSP.php
+++ themSP.php
@@ -5,58 +5,12 @@
     <meta name="viewport"
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-<!--    BOOSTRAP CSS-->
+    <!--    BOOSTRAP CSS-->
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
     <title>Document</title>
 </head>
 <body>
 <?php include("navbar_table_list.php")?>
-<?php
-if (isset($_GET['TenSP'])) {
-    $tensp = $_GET['TenSP'];
-}
-if (isset($_GET['Gia'])) {
-    $gia = $_GET['Gia'];
-}
-if (isset($_GET['Mota'])) {
-    $mota = $_GET['Mota'];
-}
-if (isset($_GET['NCC'])) {
-    $ncc = $_GET['NCC'];
-}
-
-?>
-<?php
-    $danhsachSP=[];
-    $danhsachSP[]=[
-            "name"=>"SP1",
-            "price"=>"10000",
-            "desc"=>"con hang",
-            "brand"=>"brand1"
-    ];
-    $danhsachSP[]=[
-            "name"=>"SP2",
-            "price"=>"20000",
-            "desc"=>"con hang",
-            "brand"=>"brand2"
-    ];
-    $danhsachSP[]=[
-            "name"=>"SP3",
-            "price"=>"30000",
-            "desc"=>"con hang",
-            "brand"=>"brand3"
-    ];
-
-if (isset($_GET['TenSP']) && isset($_GET['Gia']) && isset($_GET['Mota']) && isset($_GET['NCC'])) {
-
-    $danhsachSP[] = [
-        "name" => $tensp,
-        "price" => $gia,
-        "desc" => $mota,
-        "brand" => $ncc
-    ];
-}
-?>
 <table class="table">
     <thead>
     <tr>
@@ -67,15 +21,34 @@ if (isset($_GET['TenSP']) && isset($_GET['Gia']) && isset($_GET['Mota']) && isse
     </tr>
     </thead>
     <tbody>
-    <?php foreach ($danhsachSP as $ds){ ?>
-    <tr>
-            <th scope="row"><?php echo $ds["name"] ?></th>
-            <td><?php echo $ds["price"] ?></td>
-            <td><?php echo $ds["desc"] ?></td>
-            <td><?php echo $ds["brand"] ?></td>
-    </tr>
-    <?php } ?>
+        <tr>
+            <form name="formAdd" action="table_list.php" method="get">
+                <th scope="row">
+                        <input placeholder="TenSP" name="TenSP">
+                </th>
+                <td>
+                        <input placeholder="Gia" name="Gia">
+                </td>
+                <td>
+                        <input placeholder="Mo ta" name="Mota">
+                </td>
+                <td>
+                        <input placeholder="TenNCC" name="NCC">
+                </td>
+                <button style="
+                    background-color: darkblue;
+                    float:left;
+                    border: none;
+                    color: white;
+                    height: 50px;
+                    margin: 10px 10px;
+                    padding: 10px"
+                    type="submit" >
+                        Them San Pham
+                </button>
+            </form>
 
+        </tr>
     </tbody>
 </table>
 
